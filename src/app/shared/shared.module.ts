2ccf8996f319e9ddc9c5f8a7d7c95f9ec51d6302
ManfredSteyer <manfred.steyer@gmx.net>
2017-07-04 16:09:53 +0200

    validation errors comp

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { CityPipe } from "app/shared/pipes/city.pipe";
+import { FlightValidationErrorsComponent } from "app/shared/validation/flight-validation-errors/flight-validation-errors.component";
 
 @NgModule({
     imports: [
         CommonModule
     ],
     declarations: [
-        CityPipe
+        CityPipe,
+        FlightValidationErrorsComponent
     ],
     providers: [],
     exports: [
-        CityPipe
+        CityPipe,
+        FlightValidationErrorsComponent
     ]
 })
 export class SharedModule { }
---
+++ src/app/shared/validation/flight-validation-errors/flight-validation-errors.component.ts
@@ -0,0 +1,16 @@
+
+
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'flight-validation-errors',
+    templateUrl: './flight-validation-errors.component.html'
+})
+
+export class FlightValidationErrorsComponent implements OnInit {
+    constructor() { }
+
+    @Input() errors: object;
+
+    ngOnInit() { }
+}
